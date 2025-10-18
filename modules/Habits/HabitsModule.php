94926348ad52a7c9f8754f417639b2d09301570e
Arnaud Steiner <asteiner@hoplunch.com>
2025-10-18 16:31:49 +0200

    feat: added first module basic configuration

---
+++ modules/Habits/HabitsModule.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace LifeHub\Modules\Habits;
+
+use LifeHub\Core\Interface\LifeHubModuleInterface;
+
+final class HabitsModule implements LifeHubModuleInterface
+{
+
+    public static function getName(): string
+    {
+        return "Suivi des habitudes";
+    }
+
+    public static function getSlug(): string
+    {
+        return "habits";
+    }
+
+    public static function getDescription(): string
+    {
+        return "Module de gestion des habitudes";
+    }
+
+    public static function getVersion(): string
+    {
+        return "0.1.0";
+    }
+
+    public static function getIcon(): string
+    {
+        return "fa-solid fa-hourglass";
+    }
+}
