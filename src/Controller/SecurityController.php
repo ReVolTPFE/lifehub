2c02a5443e94856d388fcb7b77d1c19f10f6d80f
Arnaud Steiner <asteiner@hoplunch.com>
2025-11-12 15:20:16 +0100

    feat: added basic security with login form

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends AbstractController
+{
+    public function __construct(private readonly Security $security)
+    {
+    }
+
+    #[Route(path: '/', name: 'app_home')]
+    public function home(): Response
+    {
+        $user = $this->security->getUser();
+
+        if ($user) {
+            return $this->redirectToRoute('app_dashboard_index');
+        }
+
+        return $this->redirectToRoute('app_login');
+    }
+
+    #[Route(path: '/login', name: 'app_login')]
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        if ($this->security->getUser()) {
+            return $this->redirectToRoute('app_dashboard_index');
+        }
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error,
+        ]);
+    }
+
+    #[Route(path: '/logout', name: 'app_logout')]
+    public function logout(): Response
+    {
+        return $this->redirectToRoute('app_login');
+    }
+}
